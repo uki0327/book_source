d294f67afcac5f61f0dce51bae6e99a9d6d014fe
uki0327 <uki0327@gmail.com>
2021-01-22 17:17:26 +0900

    Update .gitignore, Project.xml, and 5149 more files...

---
+++ DoItAndroid_7th/part2/chapter13/SampleCaptureIntent/app/src/main/java/org/techtown/capture/intent/MainActivity.java
@@ -0,0 +1,116 @@
+package org.techtown.capture.intent;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
+    ImageView imageView;
+
+    File file;
+    Uri uri;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+    }
+
+    public void takePicture() {
+        try {
+            file = createFile();
+            if (file.exists()) {
+                file.delete();
+            }
+
+            file.createNewFile();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        if(Build.VERSION.SDK_INT >= 24) {
+            uri = FileProvider.getUriForFile(this, "org.techtown.capture.intent.fileprovider", file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, 101);
+        }
+    }
+
+    private File createFile() {
+        String filename = "capture.jpg";
+        File outFile = new File(getExternalCacheDir(), filename);
+
+        return outFile;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 101 && resultCode == RESULT_OK) {
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(uri));
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[],
+                                           int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions denied : " + permissions.length,
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int requestCode, String[] permissions) {
+        Toast.makeText(this, "permissions granted : " + permissions.length, Toast.LENGTH_LONG).show();
+    }
+
+}
